cf30ba3479d64fab1cfa865cf94b4f3b6476cc86
Teddy <tcbarzyk@gmail.com>
2026-02-26 23:42:09 -0500

    fixed typo

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,7 +2,7 @@ export const about = {
   paragraphs: [
     "I'm a sophomore at UNC Chapel Hill studying Computer Science and Philosophy with a minor in Music.",
     "I have experience developing full-stack web and mobile applications, with a focus on <strong>SwiftUI</strong> and <strong>React</strong>. So far, I've worked on apps for community non-profits, completed personal projects, and participated in multiple hackathons. I'm an experienced programmer and familiar with <strong>Java</strong>, <strong>Javascript</strong>, <strong>Python</strong>, <strong>C</strong>, <strong>Swift</strong>, and <strong>HTML/CSS</strong>. I also have experience working with both <strong>SQL</strong> and <strong>NoSQL (MongoDB, Firebase Firestore)</strong> databases.",
-    "My mission is promote ethical and human-centered use of technology, with a focus on AI ethics. I'm interested in exploring how agentic AI solutions can enhance life and improve productivity, while respecting human autonomy.",
+    "My mission is to promote ethical and human-centered use of technology, with a focus on AI ethics. I'm interested in exploring how agentic AI solutions can enhance life and improve productivity, while respecting human autonomy.",
     "Some of my interests include jazz piano, movies, books, baking, and playing/developing video games."
   ]
 };
